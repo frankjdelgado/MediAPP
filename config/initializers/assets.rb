8d8bac60b4968209aa1d8feaa193a9c819e1cbf6
Francisco Delgado <frankjdelgado@gmail.com>
2015-01-25 17:42:10 +0000

    sign in, sign up views

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( chui-android-3.8.1.min.css )
+Rails.application.config.assets.precompile += %w( chui-win-3.8.1.min.css )
+Rails.application.config.assets.precompile += %w( chui-ios-3.8.1.min.css )
