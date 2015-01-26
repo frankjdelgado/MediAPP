d3b949ddb0ac7026859f8ad69e59cec64fd33b3c
Francisco Delgado <frankjdelgado@gmail.com>
2015-01-26 01:44:01 +0000

    chui events

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,16 @@
 $(function() {
 
-    $('#editButton').on('$.eventStart', function() {
+    $('#addButton').on('singletap', function() {
       console.log('You pushed my button!');
     });
-
-    $('#med-list').UIEditList();
+    $('#med-list').UIEditList({
+        editLabel : 'Edit',
+        doneLabel : 'Ok',
+        deleteLabel : 'Delete',
+        callback : function(){
+          alert("Se ha terminado");
+        },
+        deletable: true,
+        movable: false
+    });
 });
